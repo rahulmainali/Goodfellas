e24e97079eb54f544983df47dd15686e4abf69a7
Rastra17 <rastradhoj@gmail.com>
2021-09-11 22:07:39 +0545

    final correction file in connection

--- Airline Management System/src/ams/connection.java
+++ Airline Management System/src/ams/connection.java
@@ -11,5 +11,15 @@ public class connection
 
     public connection()
     {
-        
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn=DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "root");
+            stat=conn.createStatement();
+        }
+        catch(Exception expt)
+        {
+            System.out.println(expt);
+        }
     }
+}

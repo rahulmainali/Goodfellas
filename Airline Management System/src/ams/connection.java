083312270903ece47bd2e1598b5e1c8416834e49
Rastra17 <rastradhoj@gmail.com>
2021-09-11 22:05:39 +0545

    some more code in connection

--- Airline Management System/src/ams/connection.java
+++ Airline Management System/src/ams/connection.java
@@ -1,2 +1,15 @@
 //A java connection file to connect to MySQL
 
+package ams;
+
+import java.sql.*;
+
+public class connection
+{
+    Connection conn;
+    Statement stat;
+
+    public connection()
+    {
+        
+    }

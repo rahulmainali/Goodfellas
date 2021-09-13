511ae775276cfe0d0ee31322fb67c1956e474891
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2021-09-13 22:20:48 +0545

    Adding code to customer file

--- Airline Management System/src/ams/Add_Customer.java
+++ Airline Management System/src/ams/Add_Customer.java
@@ -41,3 +41,49 @@ public class Add_Customer extends JFrame
         Pnrno.setFont(new Font("Tahoma", Font.PLAIN, 17));
         Pnrno.setBounds(60, 120, 150, 27);
         add(Pnrno);
+        	
+        textField_1 = new JTextField();
+        textField_1.setBounds(200, 120, 150, 27);
+        add(textField_1);
+            
+        JLabel Address = new JLabel("ADDRESS");
+        Address.setFont(new Font("Tahoma", Font.PLAIN, 17));
+        Address.setBounds(60, 170, 150, 27);
+        add(Address);
+			
+        textField_2 = new JTextField();
+        textField_2.setBounds(200, 170, 150, 27);
+        add(textField_2);
+            		
+        JLabel Nationality = new JLabel("NATIONALITY");
+        Nationality.setFont(new Font("Tahoma", Font.PLAIN, 17));
+        Nationality.setBounds(60, 220, 150, 27);
+        add(Nationality);
+			
+        textField_3 = new JTextField();
+        textField_3.setBounds(200, 220, 150, 27);
+        add(textField_3);
+	
+        JLabel Name = new JLabel("NAME");
+        Name.setFont(new Font("Tahoma", Font.PLAIN, 17));
+        Name.setBounds(60, 270, 150, 27);
+        add(Name);
+	
+        textField_4 = new JTextField();
+        textField_4.setBounds(200, 270, 150, 27);
+        add(textField_4);
+	
+        JLabel Gender = new JLabel("GENDER");
+        Gender.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        Gender.setBounds(60, 320, 150, 27);
+        add(Gender);
+		
+        JRadioButton NewRadioButton = new JRadioButton("Male");
+        NewRadioButton.setBackground(Color.WHITE);
+        NewRadioButton.setBounds(200, 320, 70, 27);
+        add(NewRadioButton);
+	
+        JRadioButton Female = new JRadioButton("Female");
+        Female.setBackground(Color.WHITE);
+        Female.setBounds(280, 320, 70, 27);
+        add(Female);

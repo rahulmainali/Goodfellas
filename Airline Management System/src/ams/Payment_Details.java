00cda45118ca2395a142ae30422ba15040935269
Rastra17 <rastradhoj@gmail.com>
2021-09-14 21:34:28 +0545

    adding code to payment details

--- Airline Management System/src/ams/Payment_Details.java
+++ Airline Management System/src/ams/Payment_Details.java
@@ -20,3 +20,30 @@ public class Payment_Details extends JFrame
     {
         new Payment_Details();
     }
+
+    private void initialize()
+    {
+        setTitle("PAYMENT_DETAILS");
+	    getContentPane().setBackground(Color.WHITE);
+	    setSize(860,486);
+	    setLayout(null);
+		
+	    JLabel Fcode = new JLabel("PNR NO");
+	    Fcode.setFont(new Font("Tahoma", Font.PLAIN, 17));
+	    Fcode.setBounds(60, 160, 150, 26);
+	    add(Fcode);
+		
+	    textField = new JTextField();
+	    textField.setBounds(200, 160, 150, 26);
+	    add(textField);
+	
+        table = new JTable();
+	    table.setBounds(45, 329, 766, 87);
+	    add(table);
+		
+	    JButton Show = new JButton("SHOW");
+	    Show.setFont(new Font("Tahoma", Font.PLAIN, 17));
+        Show.setBackground(Color.BLACK);
+        Show.setForeground(Color.WHITE);
+	    Show.setBounds(200, 210, 150, 26);
+	    add(Show);

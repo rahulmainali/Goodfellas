7c328e76d14d5f0d032192c42506edd448c2370a
AaroT47 <38485570+AaroT47@users.noreply.github.com>
2021-09-27 22:00:24 +0545

    Complete Login Page

--- Airline Management System/src/ams/Login.java
+++ Airline Management System/src/ams/Login.java
@@ -39,3 +39,109 @@ public class Login extends JFrame implements ActionListener
         t1 = new TextField(15); 
 	    t2 = new TextField(15); 
         t2.setEchoChar('*');
+        
+	    b2 = new Button("Reset");
+        b2.setFont(f2);
+		
+        b3 = new Button("Submit");
+        b3.setFont(f2);
+		
+        b4 = new Button("Close");
+	    b4.setFont(f2);
+		
+        gbc.gridx=0;
+	    gbc.gridy=0; 
+        gbl.setConstraints(l1,gbc);
+	    add(l1);
+        
+	    gbc.gridx=2;
+        gbc.gridy=0;
+	    gbl.setConstraints(t1,gbc);
+        add(t1);
+		
+	    gbc.gridx=0;
+        gbc.gridy=2;
+	    gbl.setConstraints(l2,gbc);
+        add(l2);
+
+	    gbc.gridx=2;
+        gbc.gridy=2;
+        gbl.setConstraints(t2,gbc);
+	    add(t2);
+				
+	
+	    gbc.gridx=0;
+        gbc.gridy=8;
+	    gbl.setConstraints(b2,gbc);
+        add(b2);
+
+        gbc.gridx=2;
+	    gbc.gridy=8;
+        gbl.setConstraints(b3,gbc);
+	    add(b3);
+	
+        gbc.gridx=4;
+	    gbc.gridy=8;
+        gbl.setConstraints(b4,gbc);
+	    add(b4);
+        
+
+        b2.addActionListener(this);
+        b3.addActionListener(this);
+	    b4.addActionListener(this);
+                
+        setVisible(true);   
+        setSize(400,250); 
+        setLocation(400,200);   
+    }
+
+    public void actionPerformed(ActionEvent ae)
+    {
+        if(ae.getSource()==b2)
+        {
+          
+            t1.setText("");  
+            t2.setText(""); 
+        }
+        
+        if(ae.getSource()==b4)
+        {
+            System.exit(0); 
+        }
+        
+        if(ae.getSource()==b3)
+        {
+            try
+            {
+                connection c1 = new connection();
+                
+                String s1 = t1.getText();
+                String s2 = t2.getText();
+            
+                String str = "select * from login where username = '"+s1+"' and password = '"+s2+"'";
+                ResultSet rs = c1.stat.executeQuery(str);  
+                
+                if(rs.next())
+                {
+                    new Mainframe();
+                    setVisible(false);
+                }
+                else
+                {
+                    JOptionPane.showMessageDialog(null,"Invalid Login");
+                    setVisible(false); 
+                }
+            
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+    }        
+    
+    public static void main(String[] args)
+    {
+        new Login();
+    }
+}

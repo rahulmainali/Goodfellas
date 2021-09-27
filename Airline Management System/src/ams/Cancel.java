0f8b261aa31502e963c036031d128d105196ab5a
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2021-09-27 21:29:57 +0545

    cancel additional

--- Airline Management System/src/ams/Cancel.java
+++ Airline Management System/src/ams/Cancel.java
@@ -33,3 +33,43 @@ public class Cancel extends JFrame
 	    Cancellation.setFont(new Font("Tahoma", Font.PLAIN, 31));
 	    Cancellation.setBounds(185, 24, 259, 38);
 	    add(Cancellation);
+        
+	
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("ams/Images/Cancel.png"));
+        Image i2 = i1.getImage().getScaledInstance(250, 250, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel NewLabel = new JLabel(i3);
+	    NewLabel.setBounds(470, 100, 250, 250);
+	    add(NewLabel);
+		
+	    JLabel PassengerNo = new JLabel("PASSENGER NO");
+	    PassengerNo.setFont(new Font("Tahoma", Font.PLAIN, 17));
+	    PassengerNo.setBounds(60, 100, 132, 26);
+	    add(PassengerNo);
+		
+	    JLabel CancellationNo = new JLabel("CANCELLATION NO");
+	    CancellationNo.setFont(new Font("Tahoma", Font.PLAIN, 17));
+	    CancellationNo.setBounds(60, 150, 150, 27);
+	    add(CancellationNo);
+		
+	    JLabel CancellationDate = new JLabel("CANCELLATION DATE");
+	    CancellationDate.setFont(new Font("Tahoma", Font.PLAIN, 17));
+	    CancellationDate.setBounds(60, 200, 180, 27);
+	    add(CancellationDate);
+		
+	    JLabel Ticketid = new JLabel("TICKET_ID");
+	    Ticketid.setFont(new Font("Tahoma", Font.PLAIN, 17));
+	    Ticketid.setBounds(60, 250, 150, 27);
+	    add(Ticketid);
+		
+	    JLabel Flightcode = new JLabel("FLIGHT_CODE");
+	    Flightcode.setFont(new Font("Tahoma", Font.PLAIN, 17));
+	    Flightcode.setBounds(60, 300, 150, 27);
+	    add(Flightcode);
+		
+	    JButton Cancel = new JButton("CANCEL");
+	    Cancel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        Cancel.setBackground(Color.BLACK);
+        Cancel.setForeground(Color.WHITE);
+	    Cancel.setBounds(250, 350, 150, 30);
+	    add(Cancel);
